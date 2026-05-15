a13cd2acd32e9d74cb59ac0191e40bbc6dab6ce6
Girum <girumgetulemma@gmail.com>
2026-05-15 17:58:08 +0300

    Intial commit

---
+++ .ipynb_checkpoints/run-checkpoint.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.read_csv('maternal-mortality_national_eth.csv')
+print(df.head())
+df.head(10)
+
+ 
---
+++ run.py
@@ -0,0 +1,9 @@
+import pandas as pd
+df = pd.read_csv('maternal-mortality_national_eth.csv')
+# print(df)
+df.describe()
+
+# print(df.head(all))
+
+
+ 
